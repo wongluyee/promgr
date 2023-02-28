7207833efb72624aff9ed4a2631d206ae2134ff3
joshua ng <joshuang_443@hotmail.com>
2023-02-28 17:50:24 +0900

    changed enum

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,9 @@
 class Goal < ApplicationRecord
-  enum status: { met_target: "Target Achieved", fail_target: "Target Failed", over_target: "Target Overchieved" }
+  enum status: { met_target: "Target Achieved", fail_target: "Target Failed", over_target: "Target Overchieved", in_progress: "In Progress" }
+
   belongs_to :user
+  validates :description, presence: true
+  validates :title, presence: true
+  validates :status, presence: true
 
 end
