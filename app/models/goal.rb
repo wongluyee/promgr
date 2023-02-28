e95f5a68c3cbf5bf956cdd934105be65fcc02e1a
joshua ng <joshuang_443@hotmail.com>
2023-02-28 16:55:55 +0900

    added title fields to goal

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,5 +1,5 @@
 class Goal < ApplicationRecord
-  enum status: [:incomplete, :in_progress, :complete]
-
+  enum status: { met_target: "Target Achieved", fail_target: "Target Failed", over_target: "Target Overchieved" }
   belongs_to :user
+
 end
