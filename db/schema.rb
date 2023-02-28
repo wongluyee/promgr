0dc29a5441f2ec9c49714f93c7609f2fab353cc5
joshua ng <joshuang_443@hotmail.com>
2023-02-28 16:50:47 +0900

    adding default enum to tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_065426) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_28_074233) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,7 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_065426) do
     t.string "description"
     t.string "priority"
     t.datetime "due_date"
-    t.string "status"
+    t.string "status", default: "in_progress"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
