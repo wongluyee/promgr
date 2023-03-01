79a1660e0d23b7e233c95642187b0678f5802fa9
Joseph Sandoval <joesando@gmail.com>
2023-03-01 21:25:58 +0900

    daily clock in clock out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
   def dashboard
-    # skip_authorization
-    # @users = policy_scope(User)
+    skip_authorization
+    @users = policy_scope(User)
     @user = current_user
   end
 
