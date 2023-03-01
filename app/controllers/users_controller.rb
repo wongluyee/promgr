8f535928458ca812d016baeaead0e6ec43d40006
Joseph Sandoval <joesando@gmail.com>
2023-03-01 14:46:56 +0900

    timesheets on profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def dashboard
-    @users = policy_scope(User)
+    # @users = policy_scope(User)
     @user = current_user
+  end
+
+  def show
+    @user = User.find(params[:id])
+    # authorize @user
   end
 end
