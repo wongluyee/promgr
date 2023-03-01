d0444c6356e2e1f3925c200414efaa950746ab1b
Luyee Wong <wongluyee1015@hotmail.com>
2023-03-01 11:18:38 +0900

    remove top navbar and change background color

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_02_28_084707) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,7 +57,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_084707) do
     t.string "description"
     t.string "priority"
     t.datetime "due_date"
-    t.string "status"
+    t.string "status", default: "In Progress"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
