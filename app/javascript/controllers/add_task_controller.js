f715a9db0e6614a71e4f349fe8b120aca5694b73
Lu-Yee Wong <111058709+wongluyee@users.noreply.github.com>
2023-03-02 11:42:17 +0900

    Update add_task_controller.js
    remove empty lines

--- app/javascript/controllers/add_task_controller.js
+++ app/javascript/controllers/add_task_controller.js
@@ -2,14 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add-task"
 export default class extends Controller {
-
   connect() {
     console.log("connected to add task controller")
   }
 
   addtask() {
     console.log("clicked the add task button");
-
   }
-
 }
