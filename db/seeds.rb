7357fbf0b8c6b26afab6fc138b39b4670be68b25
Elmehdi Benjdi <mehdi.benjdi@gmail.com>
2023-03-03 16:31:59 +0900

    change seeds and add profile pic

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,7 +29,7 @@ User.create!(
   # file = URI.open("https://res.cloudinary.com/wagon/image/upload/c_fill,g_face,h_200,w_200/v1673190740/pyp2lkzx5ec4awqextwq.jpg")
   # user.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
-User.create!(
+josh = User.new(
   name: "Josh",
   email: "josh@ng.com",
   job_title: "Backend Dev",
@@ -37,9 +37,10 @@ User.create!(
   is_manager: false
 )
   file = URI.open("https://avatars.githubusercontent.com/u/97093935?v=4")
-  user.photo.attach(io: file, filename: "josh.png", content_type: "image/png")
+  josh.photo.attach(io: file, filename: "josh.jpg", content_type: "image/jpg")
+  josh.save
 
-User.create!(
+joe = User.new(
   name: "Joe",
   email: "joe@luc.com",
   job_title: "Lead Dev",
@@ -47,9 +48,10 @@ User.create!(
   is_manager: false
 )
   file = URI.open("https://avatars.githubusercontent.com/u/105550930?v=4")
-  user.photo.attach(io: file, filename: "joe.png", content_type: "image/png")
+  joe.photo.attach(io: file, filename: "joe.jpg", content_type: "image/jpg")
+  joe.save
 
-User.create!(
+luyee = User.new(
   name: "Luyee",
   email: "luyee@wong.com",
   job_title: "Frontend Dev",
@@ -57,7 +59,8 @@ User.create!(
   is_manager: false
 )
 file = URI.open("https://avatars.githubusercontent.com/u/111058709?v=4")
-user.photo.attach(io: file, filename: "Luyee.png", content_type: "image/png")
+luyee.photo.attach(io: file, filename: "luyee.jpg", content_type: "image/jpg")
+luyee.save
 
 users << User.where(is_manager: false)
 # user = users.sample
