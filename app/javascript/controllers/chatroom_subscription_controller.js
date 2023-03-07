1a55528ecf0a69cd20ce7a88c6fbf52f9ccb3e5c
Luyee Wong <wongluyee1015@hotmail.com>
2023-03-07 12:18:32 +0900

    fix messages not showing bug

--- app/javascript/controllers/chatroom_subscription_controller.js
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -7,10 +7,26 @@ export default class extends Controller {
   static targets = ["messages"]
 
   connect() {
-    this.channel = createConsumer().subscriptions.create(
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+    createConsumer().subscriptions.create(
       { channel: "ChatroomChannel", id: this.chatroomIdValue },
-      { received: data => console.log(data) }
+      { received: data => this.#insertMessageAndScrollDown(data)
+      }
     )
     console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    event.target.reset()
+  }
+
+  disconnect() {
+    console.log("Unsubscribed from the chatroom")
+    this.channel.unsubscribe()
   }
 }
