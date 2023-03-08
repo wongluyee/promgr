ef40580ee4132c4fd0a2897ee2cae2952c77470e
Luyee Wong <wongluyee1015@hotmail.com>
2023-03-08 12:18:05 +0900

    style the message

--- app/services/build_slack_message_service.rb
+++ app/services/build_slack_message_service.rb
@@ -11,14 +11,14 @@ class BuildSlackMessageService
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": ":exclamation: *New task added*\n*#{task.task_title}*\n#{task.due_date.strftime("%a %b %e at %l:%M %p")}"
+          "text": ":exclamation: *New task added*\n*Title: #{task.task_title}*\n#{task.due_date.strftime("%a %b %e at %l:%M %p")}"
         }
       },
       {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": "#{task.users.pluck(:name).join(', ')} please check the task details and contact your manager if you have any questions."
+          "text": "#{task.users.pluck(:name).join(', ')} please check the task details and contact your manager if you have any questions.\n\n<https://www.promgr.tech/dashboard|View task details>"
         }
       }
     ]
