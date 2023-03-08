3bd084d232bef335f19eba3b1cc4fed4a56442ad
Luyee Wong <wongluyee1015@hotmail.com>
2023-03-08 16:14:07 +0900

    remove sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  require "sidekiq/web"
-  authenticate :user, ->(user) { user.is_manager? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
-
   devise_for :users
   root to: "users#dashboard"
 
