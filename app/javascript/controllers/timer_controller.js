0c49c0c570e881688baf9cbacbee52c553b19f1f
Joseph Sandoval <joesando@gmail.com>
2023-03-09 17:08:39 +0900

    added timer

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -5,58 +5,24 @@ export default class extends Controller {
   static targets = ["hour", "min"]
 
   connect() {
-    let hour = 0;
-    let minute = 0;
-    let second = 0;
-    let count = 0;
-    this.stopWatch(hour, minute, second, count);
+    console.log("Hello, Stimulus!", this.element.dataset.time)
+    let time = this.element.dataset.time
+    let hourTarget = this.hourTarget
+    let minTarget = this.minTarget
+    setInterval(function() {
+      let duration = Date.now() - new Date(time);
+      let minutes = Math.floor((duration / (1000 * 60)) % 60);
+      let hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+
+      hours = (hours < 10) ? "0" + hours : hours;
+      minutes = (minutes < 10) ? "0" + minutes : minutes;
+      hourTarget.innerHTML = hours
+      minTarget.innerHTML = minutes
+
+    }, 1000);
     console.log(this.hourTarget)
     console.log(this.minTarget)
 
   }
-  stopWatch(hour, minute, second, count) {
-console.log(second)
-    count++;
 
-    if (count == 100) {
-        second++;
-        count = 0;
-    }
-
-    if (second == 60) {
-        minute++;
-        second = 0;
-    }
-
-    if (minute == 60) {
-        hour++;
-        minute = 0;
-        second = 0;
-    }
-
-    let hrString = hour;
-    let minString = minute;
-    let secString = second;
-    let countString = count;
-
-    if (hour < 10) {
-        hrString = "0" + hrString;
-    }
-
-    if (minute < 10) {
-        minString = "0" + minString;
-    }
-
-    if (second < 10) {
-        secString = "0" + secString;
-    }
-
-    if (count < 10) {
-        countString = "0" + countString;
-    }
-
-    this.hourTarget.innerHTML = hrString;
-    this.minTarget.innerHTML = minString;
-    setTimeout(this.stopWatch(hour, minute, second, count), 10);
-  }
 }
