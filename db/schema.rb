1875cd3d41a80a63f8dde0ef273829a89a9ff3c4
joshua ng <joshuang_443@hotmail.com>
2023-03-09 21:19:33 +0900

    save schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.0].define(version: 2023_03_09_091826) do
-=======
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_071122) do
->>>>>>> master
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
